d263024dc60d71a257bb13d18c2ef48e994a19b1
monica22schmidt <monica22schmidt@gmail.com>
2020-02-12 15:15:55 -0600

    Full dialog app

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -3,12 +3,56 @@ package com.example.lab3milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ex_menu, m);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    public void openDialog(){
+        ExampleDialog exampleD = new ExampleDialog();
+        exampleD.show(getSupportFragmentManager(), "example dialog");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openDialog();
+            }
+        });
     }
 }
+
